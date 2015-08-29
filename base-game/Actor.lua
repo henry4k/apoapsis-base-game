d42ddb00efcd976bb415be4cc380b4fa24fe0011
Henry Kielmann <henrykielmann@gmail.com>
2015-08-29 12:52:42 +0200

    Added Actor and PhysicalActor.

---
+++ base-game/Actor.lua
@@ -0,0 +1,70 @@
+--- @classmod base-game.Actor
+--- Things that the player can take control of.
+--
+-- Extends @{core.world.WorldObject}.
+--
+-- Includes @{core.Controllable}.
+
+
+local class  = require 'middleclass'
+local Vec    = require 'core/Vector'
+local Control             = require 'core/Control'
+local Controllable        = require 'core/Controllable'
+local WorldObject         = require 'core/world/WorldObject'
+local CameraManifold      = require 'core/graphics/CameraManifold'
+local EgoCameraController = require 'core/EgoCameraController'
+
+
+local DefaultFoV = math.rad(80)
+local ZoomedFoV  = math.rad(10)
+
+
+local Actor = class('base-game/Actor', WorldObject)
+Actor:include(Controllable)
+
+
+---
+-- You probably want to extend this class, since it isn't of much use on itself.
+-- @param[type=core.graphics.RenderTarget] renderTarget
+--
+function Actor:initialize( renderTarget )
+    WorldObject.initialize(self)
+    self:initializeControllable()
+
+    Control.pushControllable(self)
+
+    self.cameraManifold = CameraManifold(renderTarget)
+    self.cameraManifold:setFieldOfView(DefaultFoV)
+
+    self.egoCameraController = EgoCameraController()
+    Control.pushControllable(self.egoCameraController)
+    local function onOrientationUpdated( self, orientation )
+        local transformation = orientation:toMatrix():scale(Vec(1,1,-1))
+        -- Invert Z axis to remain in right-handed system.
+        self.cameraManifold:setViewTransformation(transformation)
+    end
+    self.egoCameraController:addEventTarget('orientation-updated', self, onOrientationUpdated)
+
+    onOrientationUpdated(self, self.egoCameraController:getOrientation())
+end
+
+function Actor:destroy()
+    Control.popControllable(self.egoCameraController)
+    Control.popControllable(self)
+    self.cameraManifold:destroy()
+    self:destroyControllable()
+    WorldObject.destroy(self)
+end
+
+--- While pressed, the field of view set to a smaller value.
+-- @control zoom
+Actor:mapControl('zoom', function( self, absolute, delta )
+    if delta > 0 then
+        self.cameraManifold:setFieldOfView(ZoomedFoV)
+    else
+        self.cameraManifold:setFieldOfView(DefaultFoV)
+    end
+end)
+
+
+return Actor
---
+++ base-game/PhysicalActor.lua
@@ -0,0 +1,114 @@
+--- @classmod base-game.PhysicalActor
+--- Actor which exists in the physical world.
+--
+-- Extends @{base-game.Actor}.
+
+
+local class  = require 'middleclass'
+local Vec    = require 'core/Vector'
+local Quat   = require 'core/Quaternion'
+local CapsuleCollisionShape = require 'core/physics/CapsuleCollisionShape'
+local Solid                 = require 'core/physics/Solid'
+local Actor                 = require 'base-game/Actor'
+
+
+local PhysicalActor = class('base-game/PhysicalActor', Actor)
+
+
+function PhysicalActor:initialize( ... )
+    Actor.initialize(self)
+
+    local collisionShape = CapsuleCollisionShape(0.5, 2)
+    self.solid = Solid(1, Vec(0,0,0), Quat(), collisionShape)
+    self.relativeForceValue = Vec(0,0,0)
+    self.relativeForce = nil
+
+    self.cameraManifold:setAttachmentTarget(self.solid)
+end
+
+function PhysicalActor:destroy()
+    self.solid:destroy()
+    Actor.destroy(self)
+end
+
+function PhysicalActor:addRelativeForce( v )
+    local value = self.relativeForceValue
+    local force = self.relativeForce
+
+    value = value + v*100
+    if value:length() > 0.01 then
+        if not force then
+            force = self.solid:createForce()
+            self.relativeForce = force
+        end
+        force:set(value, nil, true)
+    else
+        if force then
+            force:destroy()
+            self.relativeForce = nil
+        end
+    end
+
+    self.relativeForceValue = value
+end
+
+-- @control forward
+PhysicalActor:mapControl('forward', function( self, absolute, delta )
+    if delta > 0 then
+        self:addRelativeForce(Vec(0,0,1))
+    else
+        self:addRelativeForce(Vec(0,0,-1))
+    end
+end)
+
+-- @control backward
+PhysicalActor:mapControl('backward', function( self, absolute, delta )
+    if delta > 0 then
+        self:addRelativeForce(Vec(0,0,-1))
+    else
+        self:addRelativeForce(Vec(0,0,1))
+    end
+end)
+
+-- @control left
+PhysicalActor:mapControl('left', function( self, absolute, delta )
+    if delta > 0 then
+        self:addRelativeForce(Vec(-1,0,0))
+    else
+        self:addRelativeForce(Vec(1,0,0))
+    end
+end)
+
+-- @control right
+PhysicalActor:mapControl('right', function( self, absolute, delta )
+    if delta > 0 then
+        self:addRelativeForce(Vec(1,0,0))
+    else
+        self:addRelativeForce(Vec(-1,0,0))
+    end
+end)
+
+-- @control up
+PhysicalActor:mapControl('up', function( self, absolute, delta )
+    if delta > 0 then
+        self:addRelativeForce(Vec(0,1,0))
+    else
+        self:addRelativeForce(Vec(0,-1,0))
+    end
+end)
+
+-- @control down
+PhysicalActor:mapControl('down', function( self, absolute, delta )
+    if delta > 0 then
+        self:addRelativeForce(Vec(0,-1,0))
+    else
+        self:addRelativeForce(Vec(0,1,0))
+    end
+end)
+
+---- @control jump
+--PhysicalActor:mapControl('jump', function( self, absolute, delta )
+--    if delta > 0 then
+--        self.solid:applyImpulse(Vec(0,1,0)*0.5, nil, true)
+--    end
+--end)
