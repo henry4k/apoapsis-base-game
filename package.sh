3d1605a915a5e26867b87030a309ecf5810bbdee
Henry Kielmann <henrykielmann@gmail.com>
2015-08-29 21:38:53 +0200

    Fixed packaging script

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cd "$(dirname $0)/example"
+cd "$(dirname $0)/base-game"
 zip --recurse-paths \
     --compression-method deflate -8 --suffixes '.png:.ogg' \
-    '../example.zip' '.' \
+    '../base-game.zip' '.' \
     --include '*.lua' '*.vert' '*.frag' '*.json' '*.png' '*.ogg' '*.txt'
 
