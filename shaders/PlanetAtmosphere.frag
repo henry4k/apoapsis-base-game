c1000dda1a20ea7538df79b1116390713b0a9c92
Henry Kielmann <henrykielmann@gmail.com>
2015-12-12 17:32:23 +0100

    Premultiplied alpha

--- shaders/PlanetAtmosphere.frag
+++ shaders/PlanetAtmosphere.frag
@@ -14,33 +14,20 @@ const float CloudAmount     = 1.0;
 const float TransitionWidth = 0.2;
 
 const vec3 CloudAlbedo   = vec3(1,1,1);
-const vec3 CloudSpecular = vec3(1,1,1.4);
+const vec3 CloudSpecular = vec3(.2,.2,.28);
 
 void main()
 {
-    /*
-    float ln  = dot(LightDirectionCS, OuterPositionCS);
-    float lnn = dot(LightDirectionCS, InnerPositionCS);
-    float lc  = dot(LightDirectionCS, CameraDirectionCS);
-
-    ln  = max(0, ln +0.5);
-    lnn = max(0, lnn+0.5);
-
-    float x = clamp(ln + lnn*lc, 0.0, 1.0) *
-              clamp(lc + 1.0 + 0.5, 0.0, 1.0);
-
-    gl_FragColor.rgba = vec4(lnn, lnn, lnn, 1.0);
-    */
-
     float density = texture2D(CloudSampler, TexCoord).r;
 
     vec3 lightColor = CalcLightColor(CloudAlbedo,
                                      CloudSpecular,
                                      vec3(0, 0, 1));
 
-    gl_FragColor.rgb = lightColor;
-
     float start = 1.0-CloudAmount;
     float end = max(start+TransitionWidth, 1.0);
-    gl_FragColor.a = smoothstep(start, end, density);
+    float alpha = smoothstep(start, end, density);
+
+    gl_FragColor.rgb = lightColor * alpha;
+    gl_FragColor.a = alpha;
 }
--- voxel/ScaffoldStructure/init.lua
+++ voxel/ScaffoldStructure/init.lua
@@ -3,25 +3,25 @@ local Material       = require 'core/graphics/GraphicalMaterial'
 local MeshBuffer     = require 'core/graphics/MeshBuffer'
 local Texture        = require 'core/graphics/Texture'
 local Voxel          = require 'core/voxel/Voxel'
+local VoxelAccessor  = require 'core/voxel/VoxelAccessor'
 local BlockVoxelMesh = require 'core/voxel/BlockVoxelMesh'
 local SingleVoxelStructure = require 'core/voxel/SingleVoxelStructure'
 
 
-local sideMeshBuffer =
-    MeshBuffer:load('base-game/voxel/ScaffoldStructure/Scene.json', 'Side')
-local centerMeshBuffer =
-    MeshBuffer:load('base-game/voxel/ScaffoldStructure/Scene.json', 'Center')
-local albedoTexture =
-    Texture:load('2d', 'base-game/voxel/ScaffoldStructure/Albedo.png')
-local specularTexture =
-    Texture:load('2d', 'base-game/voxel/ScaffoldStructure/Specular.png')
-local normalTexture =
-    Texture:load('2d', 'base-game/voxel/ScaffoldStructure/Normal.png')
+local sideMeshBuffer   = MeshBuffer:load('base-game/voxel/ScaffoldStructure/Scene.json', 'Side')
+local centerMeshBuffer = MeshBuffer:load('base-game/voxel/ScaffoldStructure/Scene.json', 'Center')
+local plateMeshBuffer  = MeshBuffer:load('base-game/voxel/ScaffoldStructure/Scene.json', 'Plate')
+local albedoTexture   = Texture:load('2d', 'base-game/voxel/ScaffoldStructure/Albedo.png')
+local specularTexture = Texture:load('2d', 'base-game/voxel/ScaffoldStructure/Specular.png')
+local normalTexture   = Texture:load('2d', 'base-game/voxel/ScaffoldStructure/Normal.png')
 
 
-local ScaffoldStructure =
-    class('base-game/voxel/ScaffoldStructure', SingleVoxelStructure)
+local ScaffoldStructure = class('base-game/voxel/ScaffoldStructure', SingleVoxelStructure)
 ScaffoldStructure:register()
+
+local voxelAccessor = VoxelAccessor(SingleVoxelStructure.voxelAccessor)
+voxelAccessor:addMask('plated', 1)
+ScaffoldStructure.static.voxelAccessor = voxelAccessor
 
 function ScaffoldStructure.static:setupMeshChunkGenerator( generator )
     local material = Material()
@@ -43,7 +43,18 @@ function ScaffoldStructure.static:setupMeshChunkGenerator( generator )
     voxelMesh:setMeshBuffer('-y', sideMeshBuffer)
     voxelMesh:setMeshBuffer('+z', sideMeshBuffer)
     voxelMesh:setMeshBuffer('-z', sideMeshBuffer)
+    generator:addVoxelMesh(voxelMesh)
 
+    voxelMesh = BlockVoxelMesh{ material = material,
+                                isTransparent = false }
+    voxelMesh:addBitCondition(0, 10, ScaffoldStructure.id)
+    voxelMesh:addBitCondition(10, 1, 1)
+    voxelMesh:setMeshBuffer('+x', plateMeshBuffer)
+    voxelMesh:setMeshBuffer('-x', plateMeshBuffer)
+    voxelMesh:setMeshBuffer('+y', plateMeshBuffer)
+    voxelMesh:setMeshBuffer('-y', plateMeshBuffer)
+    voxelMesh:setMeshBuffer('+z', plateMeshBuffer)
+    voxelMesh:setMeshBuffer('-z', plateMeshBuffer)
     generator:addVoxelMesh(voxelMesh)
 end
 
@@ -51,16 +62,25 @@ function ScaffoldStructure:initialize( ... )
     SingleVoxelStructure.initialize(self, ...)
 end
 
-function ScaffoldStructure:create( voxelCreator )
+function ScaffoldStructure:create( voxelCreator, isPlated )
+    self.isPlated = isPlated
+
     local voxel = Voxel()
     ScaffoldStructure.voxelAccessor:write(voxel, 'id', ScaffoldStructure.id)
+    ScaffoldStructure.voxelAccessor:write(voxel, 'plated', self.isPlated)
     voxelCreator:writeVoxel(self.origin, voxel)
 end
 
 function ScaffoldStructure:read( voxelReader )
+    local voxel = voxelReader:readVoxel(self.origin)
+    self.isPlated = voxelAccessor:read(voxel, 'plated')
 end
 
 function ScaffoldStructure:write( voxelWriter )
+    local voxel = Voxel()
+    ScaffoldStructure.voxelAccessor:write(voxel, 'id', ScaffoldStructure.id)
+    ScaffoldStructure.voxelAccessor:write(voxel, 'plated', self.isPlated)
+    voxelWriter:write(self.origin, voxel)
 end
 
 
